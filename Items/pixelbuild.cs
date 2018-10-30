4b2b33598a06cefefd669b9bcff34c07e9ade5e9
exarobibliologist <32379503+exarobibliologist@users.noreply.github.com>
2018-10-29 23:19:33 -0400

    Update Items/pixelbuild.cs

--- Items/pixelbuild.cs
+++ Items/pixelbuild.cs
@@ -13,9 +13,9 @@ namespace OPRecipes.Items
         {
             base.SetStaticDefaults();
             DisplayName.SetDefault("Build Range");
-            Tooltip.SetDefault("Bonuses:\n Block placement range massively increased\n Spelunker buff and Watch.\n Immune to NoBuilding debuff");
+            Tooltip.SetDefault("Bonuses:\n Block placement range massively increased\nSpelunker buff and Watch.\nImmune to Darkness, Blackout, Obstructed, and NoBuilding debuffs");
         }
-        
+
 		public override void SetDefaults()
         {
             item.width = 1;
@@ -24,61 +24,61 @@ namespace OPRecipes.Items
             item.rare = 6;
             item.accessory = true;
         }
-        
+
 		public override void AddRecipes()
         {
             ModRecipe recipe = new ModRecipe(mod);
 			recipe.AddIngredient(74, 1); // Platinum Coins
 			recipe.SetResult(this);
             recipe.AddRecipe();
-			
+
 			recipe = new ModRecipe(mod);
 			recipe.AddIngredient(2, 50); // Dirt
             recipe.SetResult(27, 1); // makes Acorn
             recipe.AddRecipe();
-			
+
 			recipe = new ModRecipe(mod);
 			recipe.AddIngredient(2, 50); // Dirt
             recipe.SetResult(3, 1); // makes Stone - not worth it unless you want to liquidate a bunch of dirt in your inventory
             recipe.AddRecipe();
-			
+
 			recipe = new ModRecipe(mod);
 			recipe.AddIngredient(2,50); //Dirt
 			recipe.SetResult(23, 1); //makes Gel
 			recipe.AddRecipe();
-			
+
 			recipe = new ModRecipe(mod);
 			recipe.AddIngredient(2, 50); // Dirt
             recipe.SetResult(9, 1); // makes Wood - not worth it unless you want to liquidate a bunch of dirt in your inventory
             recipe.AddRecipe();
-			
+
 			recipe = new ModRecipe(mod);
 			recipe.AddIngredient(22, 50); // Iron Bar
             recipe.SetResult(118, 1); // makes Hook - not really a cheap recipe, but saves some grinding
             recipe.AddRecipe();
-        
+
 			recipe = new ModRecipe(mod);
 			recipe.AddIngredient(704, 50); // Lead Bar
             recipe.SetResult(118, 1); // makes Hook - not really a cheap recipe, but saves some grinding
             recipe.AddRecipe();
-			
+
 			recipe = new ModRecipe(mod);
 			recipe.AddIngredient(2740, 3); // Grasshoppers
 			recipe.SetResult(2002, 1); // Turn into Worms
 			recipe.AddRecipe();
-			
+
 			recipe = new ModRecipe(mod);
 			recipe.AddIngredient(2740, 3); // Grasshoppers
 			recipe.AddIngredient(19, 1); // + Gold Bar
 			recipe.SetResult(2895, 1); // Turn into Gold Worms
 			recipe.AddRecipe();
-			
+
 			recipe = new ModRecipe(mod);
 			recipe.AddIngredient(2740, 66); // Grasshoppers
 			recipe.SetResult(2673, 1); // Turn into Truffle Worms
 			recipe.AddRecipe();
 		}
-		
+
         public override void UpdateAccessory(Player player, bool hideVisual)
         {
 			player.blockRange += 100; //[Sets how far the player can place blocks] [INT]
