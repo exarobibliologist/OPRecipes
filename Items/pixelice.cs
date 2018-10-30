381c82304eba95a9cfd9d67b37cffcfd5e19b9f1
exarobibliologist <32379503+exarobibliologist@users.noreply.github.com>
2018-10-29 23:24:23 -0400

    Update Items/pixelice.cs

--- Items/pixelice.cs
+++ Items/pixelice.cs
@@ -13,9 +13,9 @@ namespace OPRecipes.Items
         {
             base.SetStaticDefaults();
             DisplayName.SetDefault("Ice Pixel");
-            Tooltip.SetDefault("Bonuses:\n Ice Skates ability\n Tiger Climbing ability\n Immune to Chilled and Frozen");
+            Tooltip.SetDefault("Bonuses:\n Ice Skates ability\n Tiger Climbing ability\n Immune to Chilled, Frozen, and Frostburn");
         }
-        
+
 		public override void SetDefaults()
         {
             item.width = 1;
@@ -24,7 +24,7 @@ namespace OPRecipes.Items
             item.rare = 6;
             item.accessory = true;
         }
-        
+
 		public override void AddRecipes()
         {
             ModRecipe recipe = new ModRecipe(mod);
@@ -32,14 +32,14 @@ namespace OPRecipes.Items
 			recipe.SetResult(this);
             recipe.AddRecipe();
         }
-		
+
         public override void UpdateAccessory(Player player, bool hideVisual)
         {
-			player.iceSkate = true; //[Ice Skate ability or decreases ice slippiness, faster movement on ice, and falling on ice won't break it] [BOOL]
-			player.spikedBoots = 2; //[Set to 1 for slide-down walls, Set to 2 for hang-on walls] (like Tiger Climbing Gear)[INT]
-			player.buffImmune[BuffID.Chilled] = true;
-			player.buffImmune[BuffID.Frozen] = true;
-            player.buffImmune[BuffID.Frostburn] = true;
+          player.iceSkate = true; //[Ice Skate ability or decreases ice slippiness, faster movement on ice, and falling on ice won't break it] [BOOL]
+			    player.spikedBoots = 2; //[Set to 1 for slide-down walls, Set to 2 for hang-on walls] (like Tiger Climbing Gear)[INT]
+          player.buffImmune[BuffID.Chilled] = true;
+          player.buffImmune[BuffID.Frozen] = true;
+          player.buffImmune[BuffID.Frostburn] = true;
         }
     }
 }
